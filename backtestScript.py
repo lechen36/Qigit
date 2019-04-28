a3f32a9f36e76e81ebd79a794b752433cd1ba95e
Lechen <46317700+lechen36@users.noreply.github.com>
2019-04-29 07:56:31 +0800

    update setvalues

--- backtestScript.py
+++ backtestScript.py
@@ -21,6 +21,7 @@ for iSymbol in Symbollist[:10]:
     df = da.data_read(iSymbol)
     df.loc[:,'Long']=0
     #for i in df.loc['trade_date']:
+    Long_columns_index=df.columns.get_loc('Long')
     for i in range(30,len(df)):
         df_hist=df.iloc[:i,:]
         df2=df_hist[df_hist['MACDXPre']>=1]
@@ -31,28 +32,38 @@ for iSymbol in Symbollist[:10]:
             continue
         
         if (success_rate>0.6) & (df_hist.iloc[-1,:]['MACDXPre']>0) & (df['Long'].iloc[i-1]==0):
-            df['Long'].iloc[i]=1
+           # df['Long'].iloc[i]=1
+            df.iat[i,Long_columns_index]=1
             
         elif df['Long'].iloc[i-1]==1: #如果昨天已经是1了
+            
             if df['ROC-1'].iloc[i]>-2.0:
-                df['Long'].iloc[i]=2
+           #     df['Long'].iloc[i]=2
+                df.iat[i,Long_columns_index]=2
                 
         elif df['Long'].iloc[i-1]==2: #如果昨天已经是1了
             if df['ROC-2'].iloc[i]>-1.0:
-                df['Long'].iloc[i]=3
+                
+            #    df['Long'].iloc[i]=3
+                df.iat[i,Long_columns_index]=3
                 
         elif df['Long'].iloc[i-1]==3: #如果昨天已经是1了
             if df['ROC-3'].iloc[i]>1.0:
-                df['Long'].iloc[i]=4
+                
+             #   df['Long'].iloc[i]=4
+             df.iat[i,Long_columns_index]=4
         
         elif df['Long'].iloc[i-1]==4: #如果昨天已经是1了
             if df['ROC-3'].iloc[i]>1.0:
-                df['Long'].iloc[i]=5 
+              #  df['Long'].iloc[i]=5 
+              df.iat[i,Long_columns_index]=5
                 
         elif df['Long'].iloc[i-1]==5: #如果昨天已经是1了
-                df['Long'].iloc[i]=0 
+               # df['Long'].iloc[i]=0 
+                df.iat[i,Long_columns_index]=0
         else:
-            df['Long'].iloc[i]=0 
+           # df['Long'].iloc[i]=0 
+            df.iat[i,Long_columns_index]=0
             
     df['income']=0
     iClosePrice=1.0
@@ -62,8 +73,13 @@ for iSymbol in Symbollist[:10]:
             iClosePrice=df['close'].iloc[i]
             
         if (df['Long'].iloc[i]>0) & (df['Long'].iloc[i-1]>0):  #如果今天和昨天买入指标都大于0
-            df['income'].iloc[i]=(df['close'].iloc[i]-df['close'].iloc[i-1])/iClosePrice*100 #收益率
-            df['iClosePrice'].iloc[i]=iClosePrice
+            
+#            df['income'].iloc[i]=(df['close'].iloc[i]-df['close'].iloc[i-1])/iClosePrice*100 #收益率
+#            df['iClosePrice'].iloc[i]=iClosePrice
+            
+            df.iat[i,df.columns.get_loc('income')]=(df['close'].iloc[i]-df['close'].iloc[i-1])/iClosePrice*100 #收益率
+            df.iat[i,df.columns.get_loc('iClosePrice')]=iClosePrice
+
             
     
     df['incomeSum']=np.cumsum(df['income'].values)     
