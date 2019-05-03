5bdd435d6833fca2d0948e7efa06906860d697f3
lechen36 <273003635@qq.com>
2019-05-03 20:11:14 +0800

    0

--- QIsystem/Untitled-1.py
+++
@@ -1,6 +0,0 @@
-import queue
-q = queue.Queue()
-for i in range(100):
-    q.put(i)
-while not q.empty():
-    print(q.get())
--- QIsystem/Untitled-2.py
+++
@@ -1,5 +0,0 @@
-import numpy as np 
-import pandas as pd 
-a1=pd.DataFrame(data=np.random.rand(30000,100))
-b1=a1.head(5)
-0
